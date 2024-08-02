42b5026581bb9ed1bdf7be0433899b6c0b8a944e
shivali <shivalipradhan1010@gmail.com>
2024-08-02 16:54:32 +0530

    Added a gitignore file

--- formScript.js
+++ formScript.js
@@ -1,10 +1,17 @@
 const nameField = document.querySelector("#name");
 const addressField = document.querySelector("#address");
 const ageField = document.querySelector("#age");  
-const checkboxes = document.querySelectorAll(".checkbox");
+const checkboxes = document.querySelectorAll("input[type='checkbox']");
 const radioBtns = document.querySelectorAll("input[type='radio']");
 const submit = document.querySelector("#submit-btn");
 
+let warningMsg;
+
+function showWarning(id, message) {
+    
+    warningMsg = document.querySelector(`#${id}`);
+    warningMsg.innerText = message;
+}
 
 function createWarning(prevElId, msg, msgId) {
 
@@ -18,92 +25,105 @@ function createWarning(prevElId, msg, msgId) {
     }
 }
 
-function removeWarning(msgId) {
-    let checkEl = document.getElementById(msgId);
-    if(checkEl) checkEl.remove();
+// function removeWarning(msgId) {
+//     let checkEl = document.getElementById(msgId);
+//     if(checkEl) checkEl.remove();
+// }
+
+function removeWarning(id) {
+    warningMsg = document.querySelector(`#${id}`);
+    warningMsg.innerText = "";
 }
 
-function isNameValid(inputName) {
-    let regex = /^[a-zA-Z]+$/;
-    return regex.test(inputName);
-}
 
-let timeOutId;
+
+// nameField.addEventListener("input", (evt) => {
+
+//     let input = evt.target.value;
+
+//     if (input) {
+//         removeWarning("no-name");
+//         if (!isNameValid(input)) {
+//             removeWarning("short-name");
+//             createWarning("#name", "Numbers and special characters are not allowed", "invalid-name");
+//         }
+//         else if (input.length <= 3 || input.length > 30) {
+//             removeWarning("invalid-name");
+//             createWarning("#name", "Please enter 4-15 characters", "short-name");
+//         }
+//         else {
+//             removeWarning("short-name");
+//             removeWarning("invalid-name");
+//         }
+//     }
+//     else {
+//         removeWarning("short-name");
+//         removeWarning("invalid-name");
+//     }
+       
+// });
 
 nameField.addEventListener("input", (evt) => {
-
-    clearTimeout(timeOutId);
     let input = evt.target.value;
-    timeOutId = setTimeout(() => {
-
-        if(input) {
-            removeWarning("no-name");
-            if(!isNameValid(input)) {
-                removeWarning("short-name");
-                createWarning("#name", "Numbers and special characters are not allowed", "invalid-name");
-            }
-            else if(input.length <= 3 || input.length > 30) {
-                removeWarning("invalid-name");
-                createWarning("#name", "Please enter 4-15 characters", "short-name");
-            }
-            else {
-                removeWarning("short-name");
-                removeWarning("invalid-name");
-            }
+    if(input) {
+        // if(validateName(input) === "invalid") showWarning("name-warning", "Numbers and special characters are not allowed");
+        // else removeWarning("name-warning");
+        
+        if(validateName(input)) {
+            console.log("name-warning");
+            showWarning("name-warning", "Please enter 4-30 letters");
         }
-        else {
-            removeWarning("short-name");
-            removeWarning("invalid-name");
-        }
+    }
+    else removeWarning("name-warning");
        
-    }, 800);
-
 });
 
+
+// addressField.addEventListener("input", (evt) => {
+
+//     let input = evt.target.value
+
+//     if (input) {
+//         removeWarning("no-address");
+//         if (input.length < 10 || input.length > 100) {
+//             createWarning("#address", "Please enter 10-100 characters", "invalid-address");
+//         }
+//         else {
+//             removeWarning("invalid-address");
+//         }
+//     }
+//     else {
+//         removeWarning("invalid-address");
+//     }
+
+
+// });
+
 addressField.addEventListener("input", (evt) => {
-
-    clearTimeout(timeOutId);
     let input = evt.target.value;
-    timeOutId = setTimeout(() => {
-
-        if(input) {
-            removeWarning("no-address");
-            if(input.length < 10 || input.length > 100) {
-                createWarning("#address", "Please enter 10-100 characters", "invalid-address");
-            }
-            else {
-                removeWarning("invalid-address");
-            }
-        }
-        else {
-            removeWarning("invalid-address");
-        }
-       
-    }, 800);
-
+    if(input) {
+        if(!validateAddress(input)) showWarning("address-warning", "Please enter 10-100 characters");
+    }
+    
 });
 
 
 ageField.addEventListener("input", (evt) => {
 
-    clearTimeout(timeOutId);
     let input = evt.target.value;
-    timeOutId = setTimeout(() => {
 
-        if(input) {
-            removeWarning("no-age");
-            if(input < 16) {
-                createWarning("#age", "Age should be more than 16 years", "invalid-age");
-            }
-            else {
-                removeWarning("invalid-age");
-            }
+    if (input) {
+        removeWarning("no-age");
+        if (input < 16) {
+            createWarning("#age", "Age should be more than 16 years", "invalid-age");
         }
         else {
             removeWarning("invalid-age");
         }
-
-    }, 800);
+    }
+    else {
+        removeWarning("invalid-age");
+    }
 
 });
 
@@ -118,15 +138,6 @@ for(box of checkboxes) {
     });
 }
 
-function validateCheckbox() {
-    let isChecked;
-    let final = 0;
-    for(box of checkboxes) {
-        isChecked = box.checked;
-        final = final || isChecked;
-    }
-    return final;
-}
 
 for(btn of radioBtns) {
     btn.addEventListener("change", (evt) => {
@@ -138,13 +149,46 @@ for(btn of radioBtns) {
         // }
     });
 }
-function validateRadio() {
-    let selRadio = document.querySelector('input[name="gender"]:checked');
-    if(selRadio) {
+
+function validateName(name) {
+    let regex = /^[a-zA-Z]+$/;
+
+    if(!regex.test(name)) {
+        console.log("invalid");
+        return "invalid";
+    }   
+    else if(name.length <= 3 && name.length > 30) {
+        console.log("out of range");
         return true;
     }
+}
+
+function validateAddress(address) {
+    if(address >= 10 && address <= 100) return true;
+}
+
+function validateAge(age) {
+    if(age > 16) return true;
+}
+
+function validateCheckbox() {
+    let selCheckbox = document.querySelector("input[name='hobby']:checked");
+    if(selCheckbox) return true;
     return false;
 }
+
+function validateRadio() {
+    let selRadio = document.querySelector("input[name='gender']:checked");
+    if(selRadio) return true;
+    return false;
+}
+
+
+
+
+
+
+
 
 let isSubmit;
 submit.addEventListener("click", (evt) => {
